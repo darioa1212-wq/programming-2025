8ccba5e827fb92a9a7ac83dbed50fffd38497031
Dario Androsevic <dario.a1212@gmail.com>
2025-11-12 11:03:48 -0800

    first commit - added all files

---
+++ notes-1-intro.py
@@ -0,0 +1,40 @@
+# Notes - Introduction
+# 16 September
+# Dario Androsevic
+
+# Create an algorithm to solve a problem
+# Problem: Create our own chatbot
+#          DarioGPT
+
+# 1. Greet the user with a predetermined statement
+greeting = "Hello! I am a chatbot"
+
+# 1. Print the greeting
+print(greeting)
+
+# 2. Introduce the bot
+print("My name is DarioGPT.")
+print("I'm not like the other guys.")
+print("I'm completely deterministic.")
+
+# 3. Wow the user with some maths
+print("I bet you dont know what 8*8 is.")
+print("I can do it.")
+print(f"8*8 is actually {8*8}.")
+
+print("what is pi squared?")
+print("I'm smart, I can do it too.")
+print(f"It is {3.14159265359 ** 2}")
+
+# 5. Get the name of the user, store it, and
+
+# 8. See IF the user is someone specific.
+# # 8a. If they're someone specific, tell them a secret
+if user_name == "Dario":
+    print("I know that you like to eat Pizza.")
+    print("But I wont tell anyone. Shhhh 🤫")
+
+    favourite_book = input("Whats your favourite book")
+
+    if favourite_book == "Harry Potter":
+        print("I like Harry Potter, too!")
---
+++ notes-4-loops.py
@@ -0,0 +1,18 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("lightgreen")
+
+mike = turtle.Turtle()
+mike.turtlesize(50)
+
+window.exitonclick()
+
+'''
+
+## Creating Turtles
+> The names of the teenage mutant ninja turtles:
+> Blue bandana - Leonardo
+> Red bandana - Raphael
+> Purple bandana - Donatello
+> Orange bandana - Michaelangelo
